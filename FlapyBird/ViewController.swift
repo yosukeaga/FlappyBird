801d70898c15afcab46f0474d78ea6b8a2dff3fc
yosuke aga <yosuke0607@aga-no-macbook-pro.local>
2016-03-14 15:48:32 +0900

    課題

--- FlapyBird/ViewController.swift
+++ FlapyBird/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        ////SKViewの上にSKSceneがくる。詳しくは2.1
+        /// SKViewに型を変換する ViewControllerに一つあるUIViewのようなもの
+        let skView = self.view as! SKView
+        
+        skView.showsFPS = true
+        
+        skView.showsNodeCount = true
+        
+        
+        let scene = GameScene(size:skView.frame.size)
+        
+        //SKSceneクラスを継承したGameSceneをSKViewに表示
+        skView.presentScene(scene)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+        
+    }
 
 }
 
